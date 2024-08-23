80484a6e78b0da59e3d24b61cc960b927833ffae
Alex Lin <chinyinalex.lin@gmail.com>
2024-08-23 04:51:19 -0400

    added onSubmit for forms

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,18 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
   @Output() cancel_clicked = new EventEmitter<void>();
+  enteredTitle=''
+  enteredSummary=''
+  enteredDueDate=''
 
   cancel_add()
   {
     console.log('cancelling');
     this.cancel_clicked.emit();
+  }
+  onSubmit(){
+    
   }
 }
